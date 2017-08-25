b6609299cbd82d08f8fa8cd281c379f3ee3aa371
Bryce Rothhammer <rothhammer.b@gmail.com>
2017-08-24 21:30:37 -0500

    better comments

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -14,16 +14,17 @@ $(document).ready(function(){
  	var winCount = 0;
  	chooseCharacter();
 
- 	//add character info
+ 	//add character info of starting health power (hp), attack power (ap), counter attack power (cap)
  	button1.attr({"data-hp": 100, "data-ap": 15, "data-cap": 50});
  	button2.attr({"data-hp": 120, "data-ap": 10, "data-cap": 8});
  	button3.attr({"data-hp": 150, "data-ap": 9, "data-cap": 12});
  	button4.attr({"data-hp": 140, "data-ap": 12, "data-cap": 15});
 
-
+ 	//function to select user character and then choose defender
  	function chooseCharacter() {
+ 		//click handler for character buttons
 	 	$(document).on("click", ".character", function(){
-
+	 		//if character button is in the starting area move all others to enemies area
 	 		if ($(".firstClick").find(this).length > 0){
 
 	 			console.log($(this).attr("data"));
@@ -31,12 +32,13 @@ $(document).ready(function(){
 	 			$(".topTitle").text("Character Health Points "+($(this).attr("data-hp")));
 	 			
 	 			console.log(characterChosen.attr("data-hp"));
-
+	 			//for loop to remove the user character from the characterArray
 		 		for (i=0; i<4; i++){
 		 			if (characterArray[i] === $(this).attr("data")){
 		 				characterArray.splice(i, 1);
 		 			}
 		 		}
+		 		//for loop to remove the non user characters from the start and append them to enemies area
 		 		for (i=1; i<=4; i++){
 
 		 			if (characterArray.indexOf($("#button"+i).attr("data")) !== -1){
@@ -47,6 +49,7 @@ $(document).ready(function(){
 		 			}
 	 			}	
 	 		}
+	 		//if character button no in the start area then a defender has been chosen and moves to the defender area
 	 		else {
 	 			console.log($(this).attr("data"));
 	 			defenderChosen = $(this);
@@ -64,7 +67,7 @@ $(document).ready(function(){
 
  	//click handler for attack button only active when defender selected
  	$(document).on("click", ".attack", function(){
- 		
+ 		//if statement to make sure there is a character in the defender area
  		if ($(".defender").find(".character").length > 0){
 
  			attackCount++;
@@ -85,11 +88,12 @@ $(document).ready(function(){
  		}
  	});
  	
+ 	//check if defender or user was defeated and log win/loss to console
  	function winLoss() {
  		if(newDefHp <= 0){
  			winCount++;
  			if (winCount === 3){
- 				console.log("You Win!");
+ 				\
  			}
  			$(".defender").empty();
  			$(".defender").append("<h2 class = bottomTitle>Defender</h2>")
